0f8bb3269f9668251582744de3b104d4c19adb81
label <label@landteam.net>
2018-07-03 13:12:03 +0800

    調整

--- src/view/Home/index.vue
+++ src/view/Home/index.vue
@@ -4,8 +4,6 @@
   </div>
 </template>
 <script>
-import { mapActions, mapGetters } from 'vuex'
-
 export default {
     name: 'Home',
     data() {
@@ -22,15 +20,11 @@ export default {
 }
 </script>
 <style scoped>
-.layout-header-bar{
-    background: #fff;
-    box-shadow: 0 1px 1px rgba(0,0,0,.1);
-}
 .container{
   /* border: 1px solid #d7dde4; */
-  background: #f5f7f9;
-  position: relative;
-  border-radius: 4px;
-  overflow: hidden;
+  // background: #f5f7f9;
+  // position: relative;
+  // border-radius: 4px;
+  // overflow: hidden;
 }
 </style>
