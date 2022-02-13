27f9925aa6f07f60269d300ee8ad18b4cbbf12ef
lab <label@landteam.net>
2022-02-13 11:30:58 +0800

    石器傳說加入人龍成長檔

--- src/util/petData.js
+++ src/util/petData.js
@@ -1,6 +1,11 @@
 import ee from './petDataEE'
 import waterblue from './petDataWaterBlue'
+import legend from './petDataLegend'
 const petData = [
+    {
+        platform: '石器傳說',
+        data: legend
+    },
     {
         platform: '石器EE',
         data: ee
---
+++ src/util/petDataLegend.js
@@ -0,0 +1,20 @@
+const petData = [
+    {
+        name: '人龍系',
+        arr: [
+            {
+                name: '邦奇洛',
+                GPF: [26, 28, 40, 19, 21]
+            },
+            {
+                name: '利則諾頓',
+                GPF: [25, 28, 39, 21, 19]
+            },
+            {
+                name: '布魯頓',
+                GPF: [25, 28, 40, 15, 24]
+            }
+        ]
+    }
+]
+export default petData
